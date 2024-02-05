23fdc3b48eb076d386557622b4e969ce86e4cf9c
junge <jung079@nate.com>
2024-02-05 13:32:38 +0900

    first commit

---
+++ static/guide/js/all.js
@@ -0,0 +1,39 @@
+$('td').on('click', function (e) {
+    try {
+        navigator.clipboard.writeText(trim($(this).text())).then(
+            () => {
+                /* clipboard successfully set */
+            },
+            () => {
+                /* clipboard write failed */
+            }
+        );
+        navigator.clipboard.readText().then((clipText) => {
+            //   document.querySelector("td").innerText += clipText;
+            console.log('복사내용  = ' + clipText);
+        });
+    } catch (error) {
+        console.info(error);
+        console.log('로컬호스트 환경이 아닌경우 브라우저 보안설정에서 클립보드 허용이 필요합니다.');
+    }
+    // 
+    if ($(this).hasClass('page')) {
+        url = !!$(this).data('link')
+            ? $(this).data('link')
+            : `/static/guide/${SITENAME}/dist/${$(this).text().trim()}.html`;
+        window.open(url, 'page');
+        $('.currentfocus').removeClass('currentfocus')
+        $(this).addClass('currentfocus')
+    }
+});
+$('td').each(function () {
+    let _this = $(this);
+    let k = _this.text().trim().match(new RegExp(/[\da-zA-Z-_]+/));
+    if ( k != null && k[0].length === k.input.length ) {
+        url = `/static/guide/${SITENAME}/dist/${$(this).text().trim()}.html`;
+        $.ajax({
+            url: url,
+            success: () => _this.addClass('page')
+        });
+    }
+});
---
+++ static/portal/js/common.js
@@ -0,0 +1,349 @@
+// ios chrome 100vh 버그 
+var vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", vh + 'px');
+$(function () {
+	$(window).trigger("orientationchange");
+});
+$(window).bind("orientationchange", function (e) {
+	var orientation = window.orientation;
+	if (orientation == 90 || orientation == -90) {
+		setTimeout( function() {
+			var vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty("--vh", vh + 'px');
+		}, 500);
+	} else {
+		setTimeout( function() {
+			var vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty("--vh", vh + 'px');
+		}, 500);
+	}
+});
+
+var ismobile = false;
+if (window.innerWidth < 1200) {
+	ismobile = true;
+}
+$(window).resize(function () {
+	if (window.innerWidth < 1200) {
+		ismobile = true;
+	} else {
+		ismobile = false;
+	}
+});
+
+function isMobile() {
+	var UserAgent = navigator.userAgent;
+	if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// 레이어 팝업
+var currentfocus = null
+function lpopOpen(trigger, target) {
+	$(target).addClass('active');
+	$(target).attr('tabindex',0).focus()
+	currentfocus = trigger;
+	$('body').addClass('popupOpened');
+}
+
+function lpopClose(target) {
+	$('body').removeClass('popupOpened');
+	$(target).removeClass('active');
+	if(currentfocus!==null) currentfocus.focus();
+}
+$(function () {
+	// 레이어 닫기
+	$('.parent-close').on('click', function () {
+		lpopClose()
+		$(this).parent().parent().removeClass('active');
+	});
+})
+
+
+//전체메뉴
+var navhtml = $('#gnb .gnb-set > ul').clone()
+navhtml.find('div').removeAttr('id')
+$('.all-nav').html(navhtml.clone())
+$('.all-nav-toggle-close').click(function (e) {
+	e.preventDefault()
+	$('#all-gnb').removeClass('active')
+});
+// 모바일
+$('.menu-toggle').on('click', function (e) {
+	e.preventDefault()
+	$('#mobile-gnb').addClass('active')
+	$('body').addClass('ovh')
+});
+$('#mobile-gnb .nav').html(navhtml)
+
+
+// 콘텐츠 서비스
+$(function () {
+	// 달력 언어설정
+	if (typeof $.fn.datepicker != 'undefined') {
+		$.fn.datepicker.dates.ko = {
+			days: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+			daysShort: ['일', '월', '화', '수', '목', '금', '토'],
+			daysMin: ['일', '월', '화', '수', '목', '금', '토'],
+			months: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+			monthsShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+			today: '오늘',
+			clear: '삭제',
+			format: 'yyyy-mm-dd',
+			titleFormat: 'yyyy년mm월',
+			weekStart: 0,
+		};
+	}
+	inputdate = $('input.sdate,input.edate,input.date');
+	inputdate.attr('autocomplete', 'off');
+	function formatDate(num) {
+		if (!num) return "";
+		var formatNum = '';
+		// 공백제거
+		num = num.replace(/\s/gi, "");
+		try {
+			if (num.length == 8) {
+				formatNum = num.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3');
+			}
+		} catch (e) {
+			formatNum = num;
+			console.log(e);
+		}
+		formatNum = formatNum == '' ? num : formatNum
+		return formatNum;
+	}
+	// 날짜형태 변환
+	$(inputdate).on('keyup', function (event) {
+		if(event.target.value.indexOf('-') === -1 && event.target.value.length == 8){
+			event.target.value = formatDate(event.target.value)
+		}
+	});
+	// 달력
+	if (typeof $.fn.datepicker != 'undefined') {
+		inputdate
+			.datepicker({
+				format: 'yyyy-mm-dd',
+				language: 'ko',
+				todayHighlight: true,
+				autoclose: true,
+				templates: {
+					leftArrow: '<i class=datepicker-prev role=img aria-label=이전></i>',
+					rightArrow: '<i class=datepicker-next role=img aria-label=다음></i>'
+				}
+			})
+			.attr('placeholder', '연도 - 월 - 일')
+		$('.month')
+			.datepicker({
+				format: 'yyyy-mm',
+				language: 'ko',
+				todayHighlight: true,
+				autoclose: true,
+				templates: {
+					leftArrow: '<i class=datepicker-prev role=img aria-label=이전></i>',
+					rightArrow: '<i class=datepicker-next role=img aria-label=다음></i>'
+				},
+				startView: 2,
+				minViewMode: 1
+			})
+			.attr('placeholder', '연도 - 월')
+			.attr('autocomplete', 'off')
+	}
+	$('input.sdate').attr('title', '검색기간 시작일 연도 - 월 - 일');
+	$('input.edate').attr('title', '검색기간 종료일 연도 - 월 - 일');
+	$('input.month').attr('title', '검색기간 연도 - 월');
+	$('input.smonth').attr('title', '검색기간 시작월 연도 - 월');
+	$('input.emonth').attr('title', '검색기간 종료월 연도 - 월');
+	$('input.date').each(function () {
+		var title = $(this).attr('title') + ' 연도 - 월 - 일'
+		$(this).attr('title', title)
+	});
+	// 달력
+	if(!isMobile() && typeof $.fn.datepicker != "undefined"){
+		$('input.sdate,input.edate,input.date')
+			.datepicker({
+				format: "yyyy-mm-dd",
+				language: "ko",
+				todayHighlight: true,
+				autoclose:true
+			})
+			.attr('placeholder','연도 - 월 - 일')
+	}
+
+	// 탭 활성화
+	$(".js-tab button").on('click', function(){
+		var $parent = $(this).closest(".js-tab");
+		var index = $(this).parent().index();
+		$(this)
+			.attr("aria-pressed", true).parent().addClass("on")
+			.siblings().removeClass("on").find("button").attr("aria-pressed", false)
+		$parent
+			.siblings(".tab-content").find(".con").hide().eq(index).show();
+	});
+	// 이미지맵 반응형
+	if(typeof $.fn.rwdImageMaps != "undefined"){
+		$('img[usemap]').rwdImageMaps();
+	}
+	// 한글파라미터 인코딩
+	$('.linkencode area').each(function (e) {
+		_href = encodeURIComponent($(this).attr('href').split('searchWrd=')[1])
+		$(this).attr('href', $(this).attr('href').split('searchWrd=')[0] + 'searchWrd=' + _href)
+	})
+});
+
+// 게시판 반응형
+$(function () {
+	$('.bd-list').each(function (i, element) {
+		_ = $(element);
+		$(this)
+			.find('tr')
+			.each(function () {
+				$(this)
+					.find('td')
+					.each(function (index) {
+						//if ($(this).hasClass('title') || $(this).hasClass('no') || $(this).text().trim() == '') {
+						if ($(this).hasClass('title') || $(this).hasClass('no') || $(this).hasClass('no-head') || $(this).hasClass('no-data') || this.childNodes.length == 0) {
+						} else {
+							var txt = '<span class="only-m">' + _.find('th').eq(index).text() + '</span>';
+							$(this).prepend(txt);
+						}
+					});
+			});
+	});
+});
+
+
+// 테이블 스크롤 아이콘 추가
+$('.table.ovx').before('<div class=mobile-scroller-x>');
+var windowresizeDefault = (function () {
+	var timeWindow = 500; // time in ms
+	var timeout;
+	var windowresizeDefault = function (args) {
+		if ($(window).width() < 750) {
+			$('.table.ovx').attr('tabindex', 0)
+		} else {
+			$('.table.ovx').removeAttr('tabindex')
+		}
+	};
+	return function () {
+		var context = this;
+		var args = arguments;
+		clearTimeout(timeout);
+		timeout = setTimeout(function () {
+			windowresizeDefault.apply(context, args);
+		}, timeWindow);
+	};
+}());
+windowresizeDefault()
+$(window).resize(function () {
+	windowresizeDefault()
+});
+
+// 레이어팝업
+var currentfocus = null;
+function lpopOpen(trigger, target) {
+	$(target).addClass('active');
+	$(target).attr('tabindex', 0).focus();
+	currentfocus = trigger;
+	$('body').addClass('popupOpened');
+}
+function lpopClose(target) {
+	$('body').removeClass('popupOpened');
+	$(target).removeClass('active');
+	if (currentfocus !== null) currentfocus.focus();
+}
+
+
+// ** 접근성 개선 **
+// 링크 접근성
+$('#content a').each(function () {
+	if ($(this).attr('target') == '_blank' && $(this).attr('title') != '') {
+		$(this).attr('title', '새창열림');
+	}
+});
+// 게시물 이미지
+$('.dbdata img')
+.each(function (index, element) {
+	if($(this).attr('alt').trim() == ''){
+		$(this).attr('alt','')
+	}
+});
+// 접근성 타이틀 제공
+if ($('.step-agg .on').text().trim() != '') {
+	document.title = $('.step-agg .on').text() + '단계 - ' + document.title
+}
+
+
+function siteGoCheck( fm ){
+	if (!fm.link.value)
+	{
+		alert("해당사이트를 선택해주세요");
+		return false;
+	}
+	else {
+		fm.action = fm.link.value;
+		return true;
+	}
+}
+
+// 글자수 제한
+$(function() {
+	$('.characterLen').each(function(index, el) {
+		$(this).parent().append('<p class="characterLenDeco">'+$(this).data().lensize+'자 남음'+'</p>')
+		$(this).keyup(function (e){
+			var selfintroduction = $(this).val();
+			var str = $(this).val();
+			var spacecount = count(str)
+			var limit = $(this).data().lensize;
+			$(this).next().html(limit-spacecount+'자 남음');
+			if(limit < spacecount){
+				alert("글자수는 "+limit+"자를 초과할수 없습니다.");
+				// $(this).val(str.substring(0,limit))
+			}
+		})
+		.next().html(count($(this).val())+'자 남음')
+	});
+});
+
+
+
+$(function() {
+	
+	//lnb toggle
+	$(".lnb button").click(function(){
+		$(this).next().toggleClass("active");
+		if ($(this).next().hasClass('active')){
+			$(this).attr("title", "하위메뉴 닫기");
+		} else {
+			$(this).attr("title", "하위메뉴 열기");
+		}
+	});
+
+	// 윈도우 프린트
+	// $('.page-print').on('click', function () {
+	// 	window.print();
+	// });
+
+	//hgroup sns
+	$(".sns-toggle").click(function(){
+		$(".sns-wrap").addClass("active");		
+	});
+	$(".sns-wrap .close").click(function(){
+		$(".sns-wrap").removeClass("active");		
+	});
+
+	//전체 메뉴  
+	$(".all-nav-toggle").click(function(){
+		$(".total-nav").addClass("active");		
+	});
+
+	//클릭시 레이어 닫기
+
+	
+
+
+
+	
+});
