5094931517a37abbc069dbe967573704d5da3dd2
BOKNET\exitps99 <hauuumi@gmail.com>
2024-04-18 13:29:34 +0900

    .

--- static/museum/js/common.js
+++ static/museum/js/common.js
@@ -26,19 +26,18 @@ if (window.innerWidth < 1200) {
 }
 if( window.innerWidth < 750 ) {
 	$(".total-nav").addClass("m-total-nav");
-	$(".total-nav .dp1 > li > a").on("click", function(e){
+	$(".total-nav.m-total-nav .dp1 > li > a").on("click", function(e){
 		e.preventDefault();
 		$(this).parent("li").toggleClass("on");
 	})
 } 
-
 $(window).resize(function () {
 	if (window.innerWidth < 1200) {
 		ismobile = true;
 		
 		if( window.innerWidth < 750 ) {
 			$(".total-nav").addClass("m-total-nav");
-			$(".total-nav .dp1 > li > a").on("click", function(e){
+			$(".total-nav.m-total-nav .dp1 > li > a").on("click", function(e){
 				e.preventDefault();
 			})
 		} 
