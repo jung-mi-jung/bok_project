4ec5798593545e777c278c80a599bc1e1963e3bd
BOKNET\exitps99 <hauuumi@gmail.com>
2024-04-22 11:00:14 +0900

    css 수정

--- static/imer/js/common.js
+++ static/imer/js/common.js
@@ -670,6 +670,10 @@ $(function() {
 	// 일반형 게시판 상세
 	$(".bd-view").parents("#content").addClass("bd-view_wrap");
 	$(".bd-view").parents("body").addClass("bd-body");
+	// 블로그형 
+	//$(".bd-blog").parents("#content").addClass("bd-blog_wrap");
+	//$(".bd-blog").parents("body").addClass("bd-body");
+
 
 
 	//셀렉트 레이어형 검색 sh-select
